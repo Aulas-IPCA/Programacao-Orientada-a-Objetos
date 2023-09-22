4205752bebb5ccbee58dd2f0db209e0643e3844a
Thiago Yabuki <a24207@alunos.ipca.pt>
2023-09-22 21:18:49 +0100

    Exercicios propostos em aula

--- Aulas/Aula 1.cs
+++ Aulas/Aula 1.cs
@@ -1,8 +1,84 @@
 ﻿
+//Alinhamento à direita
+Console.WriteLine("Alinhamento à direita");
 Console.WriteLine("Playboi Carti".PadRight(20) + "Taboão da Serra".PadRight(20));
 Console.WriteLine("Kanye West".PadRight(20) + "Campo Limpo".PadRight(20));
-Console.WriteLine("Travis Scott".PadRight(20) + "Astroworld".PadRight(20));
+Console.WriteLine("Travis Scott".PadRight(20) + "Astroworld\n".PadRight(20));
 
+//Alinhamento à esquerda
+Console.WriteLine("Alinhamento à esquerda");
 Console.WriteLine("Playboi Carti".PadLeft(20) + "Taboão da Serra".PadLeft(20));
 Console.WriteLine("Kanye West".PadLeft(20) + "Campo Limpo".PadLeft(20));
-Console.WriteLine("Travis Scott".PadLeft(20) + "Astroworld".PadLeft(20));
+Console.WriteLine("Travis Scott".PadLeft(20) + "Astroworld\n".PadLeft(20));
+
+//Tabulação
+Console.WriteLine("Tabulação");
+Console.WriteLine("Playboi Carti\t Taboão da Serra");
+Console.WriteLine("Kanye West\t Campo Limpo");
+Console.WriteLine("Travis Scott\t Astroworld\n");
+
+//Operações aritméticas
+Console.WriteLine("Operações aritméticas");
+Console.WriteLine(4 + 3 * (2 + 9));
+Console.WriteLine(45/5);
+Console.WriteLine((40%3) + "\n");
+
+//Funções matemáticas
+Console.WriteLine("Funções matemáticas");
+Console.WriteLine(Math.Pow(5,2));
+Console.WriteLine(Math.Cos(20));
+Console.WriteLine(Math.Sin(20));
+Console.WriteLine(Math.Sqrt(125));
+Console.WriteLine(Math.Round(Math.Cos(20)) + "\n");
+
+//Operações lógicas
+Console.WriteLine("Operações lógicas");
+Console.WriteLine(4==5);
+Console.WriteLine(4!=6);
+Console.WriteLine(4>6);
+Console.WriteLine((4 < 5) && (6 > 10));
+Console.WriteLine(40 > 50 || 60 > 90);
+Console.WriteLine(!(40 > 50 || 60 > 90) + "\n");
+
+//Operações alfanúmericas
+
+
+//Numeros inteiros
+
+
+//Conversão de inteiros para reais e vice-versa
+
+
+//Conversão de alfanúmericos para númericos
+
+
+//Arredondamento de números reais
+
+
+//Percentagens
+
+
+//Datas cronológicas
+
+
+//Formatos númericos
+
+
+//Operadores combinados
+
+
+//Leitura de dados alfanúmericos
+
+
+//Leitura de dados númericos
+
+
+//Maior de dois
+
+
+//Multibanco
+
+
+//Números ímpares
+
+
