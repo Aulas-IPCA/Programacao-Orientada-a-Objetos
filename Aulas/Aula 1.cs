b6383520514923317cab2b5ca99b981869c13256
Thiago Yabuki <a24207@alunos.ipca.pt>
2023-09-22 20:45:44 +0100

    exercicios de alinhamento

--- Aulas/Aula 1.cs
+++ Aulas/Aula 1.cs
@@ -1,1 +1,8 @@
 ﻿
+Console.WriteLine("Playboi Carti".PadRight(20) + "Taboão da Serra".PadRight(20));
+Console.WriteLine("Kanye West".PadRight(20) + "Campo Limpo".PadRight(20));
+Console.WriteLine("Travis Scott".PadRight(20) + "Astroworld".PadRight(20));
+
+Console.WriteLine("Playboi Carti".PadLeft(20) + "Taboão da Serra".PadLeft(20));
+Console.WriteLine("Kanye West".PadLeft(20) + "Campo Limpo".PadLeft(20));
+Console.WriteLine("Travis Scott".PadLeft(20) + "Astroworld".PadLeft(20));
