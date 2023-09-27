8849328157589a023b53a122ab0035c45ce2fbd8
Thiago Yabuki <a24207@alunos.ipca.pt>
2023-09-27 19:15:23 +0100

    exercicios feitos em aula

--- Aulas/Aula 1.cs
+++ Aulas/Aula 1.cs
@@ -1,128 +1,137 @@
-﻿
-//Alinhamento à direita
-Console.WriteLine("Alinhamento à direita");
-Console.WriteLine("Playboi Carti".PadRight(20) + "Taboão da Serra".PadRight(20));
-Console.WriteLine("Kanye West".PadRight(20) + "Campo Limpo".PadRight(20));
-Console.WriteLine("Travis Scott".PadRight(20) + "Astroworld\n".PadRight(20));
+﻿using System;
 
-//Alinhamento à esquerda
-Console.WriteLine("Alinhamento à esquerda");
-Console.WriteLine("Playboi Carti".PadLeft(20) + "Taboão da Serra".PadLeft(20));
-Console.WriteLine("Kanye West".PadLeft(20) + "Campo Limpo".PadLeft(20));
-Console.WriteLine("Travis Scott".PadLeft(20) + "Astroworld\n".PadLeft(20));
+namespace Aula_1
+{
+    class Aula1
+    {
+        static void Main()
+        {
+            //Alinhamento à direita
+            Console.WriteLine("Alinhamento à direita");
+            Console.WriteLine("Playboi Carti".PadRight(20) + "Taboão da Serra".PadRight(20));
+            Console.WriteLine("Kanye West".PadRight(20) + "Campo Limpo".PadRight(20));
+            Console.WriteLine("Travis Scott".PadRight(20) + "Astroworld\n".PadRight(20));
 
-//Tabulação
-Console.WriteLine("Tabulação");
-Console.WriteLine("Playboi Carti\t Taboão da Serra");
-Console.WriteLine("Kanye West\t Campo Limpo");
-Console.WriteLine("Travis Scott\t Astroworld\n");
+            //Alinhamento à esquerda
+            Console.WriteLine("Alinhamento à esquerda");
+            Console.WriteLine("Playboi Carti".PadLeft(20) + "Taboão da Serra".PadLeft(20));
+            Console.WriteLine("Kanye West".PadLeft(20) + "Campo Limpo".PadLeft(20));
+            Console.WriteLine("Travis Scott".PadLeft(20) + "Astroworld\n".PadLeft(20));
 
-//Operações aritméticas
-Console.WriteLine("Operações aritméticas");
-Console.WriteLine(4 + 3 * (2 + 9));
-Console.WriteLine(45/5);
-Console.WriteLine((40%3) + "\n");
+            //Tabulação
+            Console.WriteLine("Tabulação");
+            Console.WriteLine("Playboi Carti\t Taboão da Serra");
+            Console.WriteLine("Kanye West\t Campo Limpo");
+            Console.WriteLine("Travis Scott\t Astroworld\n");
 
-//Funções matemáticas
-Console.WriteLine("Funções matemáticas");
-Console.WriteLine(Math.Pow(5,2));
-Console.WriteLine(Math.Cos(20));
-Console.WriteLine(Math.Sin(20));
-Console.WriteLine(Math.Sqrt(125));
-Console.WriteLine(Math.Round(Math.Cos(20)) + "\n");
+            //Operações aritméticas
+            Console.WriteLine("Operações aritméticas");
+            Console.WriteLine(4 + 3 * (2 + 9));
+            Console.WriteLine(45 / 5);
+            Console.WriteLine((40 % 3) + "\n");
 
-//Operações lógicas
-Console.WriteLine("Operações lógicas");
-Console.WriteLine(4==5);
-Console.WriteLine(4!=6);
-Console.WriteLine(4>6);
-Console.WriteLine((4 < 5) && (6 > 10));
-Console.WriteLine(40 > 50 || 60 > 90);
-Console.WriteLine(!(40 > 50 || 60 > 90) + "\n");
+            //Funções matemáticas
+            Console.WriteLine("Funções matemáticas");
+            Console.WriteLine(Math.Pow(5, 2));
+            Console.WriteLine(Math.Cos(20));
+            Console.WriteLine(Math.Sin(20));
+            Console.WriteLine(Math.Sqrt(125));
+            Console.WriteLine(Math.Round(Math.Cos(20)) + "\n");
 
-//Operações alfanúmericas
-Console.WriteLine("Operações alfanúmericas");
-Console.WriteLine("Hello, World!".Length);
-Console.WriteLine("Hello, " + "World");
-Console.WriteLine("Hello, World!".ToUpper()); //caps
-Console.WriteLine("Hello, World!".Substring(3, 5)); //mudar de posição
-Console.WriteLine("Hello, World!".ToLower()); //minusculo
-Console.WriteLine("      Hello, World!    ".Trim()); //remove os espaços
-Console.WriteLine("Hello, World!".CompareTo("World"));
-string name = "Hello, World!".Split(",")[0];
-string name2 = "Hello, World!".Split(',')[1];
-Console.WriteLine(name);
-Console.WriteLine(name2);
-Console.WriteLine("Hello, World!\n" == "Hello, World!\n");
-Console.WriteLine();
+            //Operações lógicas
+            Console.WriteLine("Operações lógicas");
+            Console.WriteLine(4 == 5);
+            Console.WriteLine(4 != 6);
+            Console.WriteLine(4 > 6);
+            Console.WriteLine((4 < 5) && (6 > 10));
+            Console.WriteLine(40 > 50 || 60 > 90);
+            Console.WriteLine(!(40 > 50 || 60 > 90) + "\n");
 
-//Numeros inteiros
-Console.WriteLine("Números inteiros");
-sbyte S = 34;
-byte B = 250;
-short S2 = 32000;
-ushort U = 10002;
-int UI = 35000004;
-uint UI2 = 3500000;
-long L = 12345678900;
+            //Operações alfanúmericas
+            Console.WriteLine("Operações alfanúmericas");
+            Console.WriteLine("Hello, World!".Length);
+            Console.WriteLine("Hello, " + "World");
+            Console.WriteLine("Hello, World!".ToUpper()); //caps
+            Console.WriteLine("Hello, World!".Substring(3, 5)); //mudar de posição
+            Console.WriteLine("Hello, World!".ToLower()); //minusculo
+            Console.WriteLine("      Hello, World!    ".Trim()); //remove os espaços
+            Console.WriteLine("Hello, World!".CompareTo("World"));
+            string name = "Hello, World!".Split(",")[0];
+            string name2 = "Hello, World!".Split(',')[1];
+            Console.WriteLine(name);
+            Console.WriteLine(name2);
+            Console.WriteLine("Hello, World!\n" == "Hello, World!\n");
+            Console.WriteLine();
 
-Console.WriteLine(S + "<" + B + "<" + S2 + "<" + U);
-Console.WriteLine(UI + "<" + UI2 + "<" + L + "\n");
+            //Numeros inteiros
+            Console.WriteLine("Números inteiros");
+            sbyte S = 34;
+            byte B = 250;
+            short S2 = 32000;
+            ushort U = 10002;
+            int UI = 35000004;
+            uint UI2 = 3500000;
+            long L = 12345678900;
 
-//Conversão de inteiros para reais e vice-versa
-Console.WriteLine("Conversão de inteiros para reais e vice-versa");
+            Console.WriteLine(S + "<" + B + "<" + S2 + "<" + U);
+            Console.WriteLine(UI + "<" + UI2 + "<" + L + "\n");
 
-Double D = 99.56;
-int I = (int)D;
+            //Conversão de inteiros para reais e vice-versa
+            Console.WriteLine("Conversão de inteiros para reais e vice-versa");
 
-Console.WriteLine(I + "->" + D);
-Console.WriteLine(I + "->" + I);
-Console.WriteLine();
+            Double D = 99.56;
+            int I = (int)D;
 
-//Conversão de alfanúmericos para númericos
-Console.WriteLine("Conversão de alfanúmericos para númericos");
+            Console.WriteLine(I + "->" + D);
+            Console.WriteLine(I + "->" + I);
+            Console.WriteLine();
 
-string num = "99";
+            //Conversão de alfanúmericos para númericos
+            Console.WriteLine("Conversão de alfanúmericos para númericos");
 
-int INT = Convert.ToInt32(num);
+            string num = "99";
 
-INT = INT + 10;
+            int INT = Convert.ToInt32(num);
 
-Console.WriteLine(INT);
+            INT = INT + 10;
 
-string X = Convert.ToString(INT);
-Console.WriteLine(X + " tem " + X.Length + " dígitos.\n");
+            Console.WriteLine(INT);
+
+            string X = Convert.ToString(INT);
+            Console.WriteLine(X + " tem " + X.Length + " dígitos.\n");
 
 
-//Arredondamento de números reais
-Console.WriteLine("Arredondamento de números reais");
-Console.WriteLine("{0, 4:F2}", 4.789675);
-Console.WriteLine();
+            //Arredondamento de números reais
+            Console.WriteLine("Arredondamento de números reais");
+            Console.WriteLine("{0, 4:F2}", 4.789675);
+            Console.WriteLine();
 
-//Percentagens
-Console.WriteLine("Percentagens");
+            //Percentagens
+            Console.WriteLine("Percentagens");
 
-//Datas cronológicas
-Console.WriteLine("Datas cronológicas");
+            //Datas cronológicas
+            Console.WriteLine("Datas cronológicas");
 
-//Formatos númericos
-Console.WriteLine("Formatos númericos");
+            //Formatos númericos
+            Console.WriteLine("Formatos númericos");
 
-//Operadores combinados
-Console.WriteLine("Operadores combinados");
+            //Operadores combinados
+            Console.WriteLine("Operadores combinados");
 
-//Leitura de dados alfanúmericos
-Console.WriteLine("Leitura de dados alfanúmericos");
+            //Leitura de dados alfanúmericos
+            Console.WriteLine("Leitura de dados alfanúmericos");
 
-//Leitura de dados númericos
-Console.WriteLine("Leitura de dados númericos");
+            //Leitura de dados númericos
+            Console.WriteLine("Leitura de dados númericos");
 
-//Maior de dois
-Console.WriteLine("Maior de dois");
+            //Maior de dois
+            Console.WriteLine("Maior de dois");
 
-//Multibanco
-Console.WriteLine("Multibanco");
+            //Multibanco
+            Console.WriteLine("Multibanco");
 
-//Números ímpares
-Console.WriteLine("Números ímpares");
-
+            //Números ímpares
+            Console.WriteLine("Números ímpares");
+        }
+    }
+}
