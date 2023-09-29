ff21704000bc9d999134c7777dce90d549e8a581
Thiago Yabuki <a24207@alunos.ipca.pt>
2023-09-29 21:58:07 +0100

    exercicio herança feito em aula

---
+++ ExHerança/Aluno.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExHerança
+{
+    internal class Aluno : Pessoa
+    {
+        //Atributos
+        public string Matricula { get; set; }
+        public string Curso { get; set; }
+
+        //Construtor
+        public Aluno(string nome, int idade, string matricula, string curso) : base(nome, idade)
+        {
+            Matricula = matricula;
+            Curso = curso;
+        }
+
+        public void Estudar()
+        {
+            Console.WriteLine("O {0} está estudando a disciplina {1} e tem a matrícula {2}.", Nome, Curso, Matricula);
+        }
+    }
+}
---
+++ ExHerança/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ExHerança
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno a = new Aluno("Filipe", 29, "123456", "POO");
+
+            a.Apresentar();
+            Console.WriteLine("Matrícula: {0}", a.Matricula);
+
+            a.Estudar();
+        }
+    }
+}
