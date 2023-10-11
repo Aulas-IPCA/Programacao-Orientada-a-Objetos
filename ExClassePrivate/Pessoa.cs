34b94c591101b473d7312c8795d50c875732c0eb
Thiago Yabuki <a24207@alunos.ipca.pt>
2023-10-11 18:38:14 +0100

    ex classe private

---
+++ ExClassePrivate/Pessoa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExClassePrivate
+{
+    internal class Pessoa
+    {
+        private string nome;
+        private int idade;
+
+        public Pessoa(string nome, int idade)
+        {
+            this.nome = nome;
+            this.idade = idade;
+        }
+
+        //Metodo publico para exibir um nome (acessa a um atributo privado)
+        public void MostraNome()
+        {
+            Console.WriteLine("Nome: {0}", nome);
+        }
+        //Metodo publico para exibir uma idade
+        public void MostraIdade()
+        {
+            Console.WriteLine("Idade: {0} anos", idade);
+        }
+    }
+}
---
+++ ExClassePrivate/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ExClassePrivate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa person1 = new Pessoa("Thiago", 22);
+            Pessoa person2 = new Pessoa("Isabela", 20);
+
+            person1.MostraNome();
+            person1.MostraIdade();
+            person2.MostraNome();
+            person2.MostraIdade();
+        }
+    }
+}
